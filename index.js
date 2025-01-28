7d0e89885fc77d25309dc3e86659052aa982cb46
ADISH BAHATKAR <97829093+Adish21@users.noreply.github.com>
2025-01-28 13:48:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SentimentAnalysis from './SentimentAnalysis';
+import './App.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <SentimentAnalysis />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
